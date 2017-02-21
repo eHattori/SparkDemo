71222bc72aab455587a22ba55e0331d218111834
Eduardo Hattori <eduardo.hattori@hotmail.com>
2017-02-21 20:25:42 -0300

    Gradle Run configurado para Spark

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,9 +2,10 @@ import static spark.Spark.*;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {			
 		
-		get("/hello", (req, res) -> "\n\n Hello bandi loko \n\n\n");
-		
+		get("/hello", (req, res) -> { 			
+			return "\n\n HelloWorld \n\n\n" ;
+		});		
 	}
 }
